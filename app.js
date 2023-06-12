0b9cf10c8daca5973a5f69f5f64396185f186a34
Metranohm <andrewwink@gmail.com>
2023-06-12 17:16:11 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
